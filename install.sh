45fc163c04c908cf2df62dbcab5a3b567ebd7d2b
Phu Nguyen <tylernguyen@gmail.com>
2020-10-20 15:30:05 -0500

    Updated installer to download the binary

--- install.sh
+++ install.sh
@@ -3,18 +3,79 @@
 # location to where the basher executable and config will be stored
 destination="$HOME/.basher"
 
+# script name
+basher="$destination/basher"
+
 # source repo (raw content)
-source= "https://raw.githubusercontent.com/siphu/basher/develop"
+sourceGit="https://raw.githubusercontent.com/siphu/basher/develop"
 
 
-# create the folder
-mkdir -p "$destination"
+rm -rf "$destination"
 
 
-result= wget -O /dev/null -q "$source/bin/basher"
 
-if [result]; then
-	echo "YAY"
-elif
-	echo "FILE DOES NOT EXISTS"
-end
+main() {
+
+
+	# create the folder
+	mkdir -p "$destination"
+
+	bashUrl="$sourceGit/bin/basher"
+	result=`wget -q -S --spider $bashUrl  2>&1`
+
+	if  validateUrl $bashUrl; then
+		downloadBasher $bashUrl
+		injectRC
+		echo "Basher installed to: $basher"
+	else
+		echo "ERROR: Unable to find basher source file"
+		exit 1
+	fi
+
+
+
+
+}
+
+
+function validateUrl() {
+  if [[ `wget -S --spider $1  2>&1 | grep 'HTTP/1.1 200 OK'` ]];
+  	then return 0
+	else return 1
+  fi
+}
+
+
+function downloadBasher()
+{
+		wget -q "$1" -O "$basher"
+		chmod +x "$basher"
+}
+
+
+function injectRC()
+{
+	exportPath="export PATH=\$PATH:$destination"
+	rcFiles=(
+			"$HOME/.zshrc"
+			"$HOME/.bash_profile"
+			"$HOME/.bashrc"
+			"$HOME/.profile"
+		  )
+
+	for i in "${rcFiles[@]}"
+	do
+		if [ -f $i ]; then
+			if [ `grep -Fq "$exportPath" $i > /dev/null; echo $?`  == 1 ];  then
+				echo -e "\n$exportPath\n" >> $i
+			fi
+			`$exportPath`
+			break
+		fi
+	done
+
+
+}
+
+
+main
